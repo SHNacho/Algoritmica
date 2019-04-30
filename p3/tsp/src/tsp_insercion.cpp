5300d0fc9d47cf88461eff5820810aae4bea9a3f
SHNacho <nachosanchezh@hotmail.com>
2019-04-30 16:34:11 +0200

    Busqueda de la ciudad más cercana

--- p3/tsp/src/tsp_insercion.cpp
+++ p3/tsp/src/tsp_insercion.cpp
@@ -9,10 +9,6 @@
 
 using namespace std;
 
-int minimo(vector<double> v){
-    
-}
-
 int main(int argc, char **argv){
     const double INF = numeric_limits<double>::max();
 
@@ -23,7 +19,7 @@ int main(int argc, char **argv){
     
     ifstream input_file(argv[1]);
     string line;
-    int dimension;
+    int num_ciudades;
 
     int ciudad;
     double coor_x, coor_y;
@@ -41,10 +37,10 @@ int main(int argc, char **argv){
 
     getline(input_file, line);
     line.erase(0, 11);
-    dimension = atoi(line.c_str());
+    num_ciudades = atoi(line.c_str());
     
     //Reservamos espacio para el grafo
-    matriz <double> m(dimension, dimension, INF);
+    matriz <double> distancias(num_ciudades, num_ciudades, INF);
 
     //Movemos el offset al comienzo de los datos
     for(int i=0; i<3; ++i){
@@ -52,7 +48,7 @@ int main(int argc, char **argv){
     }
 
     //Tomamos los datos
-    for (int i=0; i<dimension; ++i){
+    for (int i=0; i<num_ciudades; ++i){
         input_file >> ciudad;
         input_file >> coor_x;
         input_file >> coor_y;
@@ -63,16 +59,16 @@ int main(int argc, char **argv){
 
     //Calculamos las distancias y las metemos en la matriz que representa
     //el grafo
-    for(int i=0; i<dimension; ++i){
-        for (int j=i+1; j<dimension; ++j){
-            m(i,j)=sqrt(pow(v_coordenadas[j].first + v_coordenadas[i].first, 2) +
+    for(int i=0; i<num_ciudades; ++i){
+        for (int j=i+1; j<num_ciudades; ++j){
+            distancias[i][j]=sqrt(pow(v_coordenadas[j].first + v_coordenadas[i].first, 2) +
                         pow(v_coordenadas[j].second + v_coordenadas[i].second, 2));
-            m(j,i)=m(i,j);
+            distancias[j][i]=distancias[i][j];
         }
     }
 
     //Generamos vector de candidatos
-    for(int i=0; i<dimension; ++i)
+    for(int i=0; i<num_ciudades; ++i)
         candidatos.push_back(i);
     
     //Elegimos el recorrido inicial
@@ -81,7 +77,7 @@ int main(int argc, char **argv){
     double mas_al_O = v_coordenadas[0].second; 
     double mas_al_N = v_coordenadas[0].first;
     
-    for(int i=1; i<dimension; ++i){
+    for(int i=1; i<num_ciudades; ++i){
         if(v_coordenadas[i].first > mas_al_N){
             mas_al_N = v_coordenadas[i].first;
             N = i;
@@ -96,20 +92,38 @@ int main(int argc, char **argv){
         }
     }
     
-    solucion.push_back(O);
-    solucion.push_back(N);
-    solucion.push_back(E);
+    solucion.push_back(O); candidatos[O] = -1;
+    solucion.push_back(N); candidatos[N] = -1;
+    solucion.push_back(E); candidatos[E] = -1;
     
     //Comienza el algoritmo
-    int insertados = 3;
-    while(insertados < dimension){
+    int tam_solucion = 3;    
+    while(tam_solucion < num_ciudades){
         
-        int minimo = 0;
-        
-        for(int i=0; i<insertados; ++i){
-            for (int j=0; j<dimension; ++j){
+        //Buscamos la ciudad más cercana al conjunto solución
+        int ciudad_origen = 0;
+        int ciudad_mas_cercana = 0;
+        double distancia_mas_cercana = INF;
+
+        for(int i=0; i<tam_solucion; ++i){
+            int ciudad = solucion[i];
+            for (int j=0; j<num_ciudades; ++j){
+                if (distancias[ciudad][j] < distancia_mas_cercana && candidatos[j] != -1){
+                    ciudad_origen = ciudad;
+                    ciudad_mas_cercana = j;
+                    distancia_mas_cercana = distancias[ciudad][j];
+                }
             }
         }
+
+        //Una vez encontrada vemos en que posición del conjunto solución insertarla
+        //para minimizar el trayecto
+        if(ciudad_origen == 0){
+        }
+
+        candidatos[ciudad_mas_cercana] = -1;
+        tam_solucion++;
+
     }
     
 
