91bfbdef05cceac873959d100ee4e3e0046967ea
adriancarmona999 <adriancarmona999@gmail.com>
2019-05-03 15:38:33 +0100

    hi bebes

--- p3/tsp/src/tsp_cercania.cpp
+++ p3/tsp/src/tsp_cercania.cpp
@@ -11,7 +11,16 @@ using namespace std;
 const double INF = numeric_limits<double>::max();
 
 
-// Devuelve la posicion dentro del vector del elemento mas pequeño.
+/*  FUNCION
+ *  Esta funcion recibe como parametros un vector, que son las distintas distancias
+ * a las distintas ciudades desde una misma ciudad y el vector candidatos para comprobar 
+ * si ya se han visitado las respectivas ciudades.
+ * 
+ * La funcion encuentra el menor elemento del vector v y devuelve la posicion dentro
+ * del vector de dicho elemento.
+ 
+ 
+ */
 int BuscaMenor(vector<double> v, vector<int> & candidatos){
   int menor;
   double minimo = INF;
@@ -78,19 +87,32 @@ int main(int argc, char **argv){
     //ALgoritmo basado en cercanias. La idea es ir a la ciudad mas cercana
     //sin formar un ciclo. Exceptuando la ultima.
 
+    // Declaramos los vectores que albergaran los conjuntos Candidato y Solucion
     vector<int> solucion;
     vector<int> candidatos;
 
+    // Inicializamos el conjunto de candidatos, el rango sera [0,15]. 
     for(int i = 0; i<dimension;++i){
       candidatos.push_back(i);
     }
 
+    // Abergamos la primera ciudad en el conjunto solucion.
     int i = 0;
     solucion.push_back(0);
     candidatos[0] = -1;
     
+    // variable donde guardaremos el indice, es decir, la ciudad a donde nos dirigimos.
     int menor;
 
+    /*CUERPO DEL ALGORITMO:
+     * La idea es encontrar la ciudad mas cercana haciendo uso de la matriz de
+     * distancias. Una vez encontrada la ciudad (indice) al que nos dirigimos,
+     * la posicion candidatos[indice] lo hacemos -1 para mostrar que esa ciudad
+     * ya la hemos visitado y introducimos el indice en el vector de soluciones.
+     * 
+     * Para concluir, asignamos el valor del indice a la variable i para empezar
+     * de nuevo todo el proceso
+     */
     while(solucion.size()< dimension   ){
         vector <double> c;
         m.get_Fila(i,c);
@@ -100,33 +122,29 @@ int main(int argc, char **argv){
       i = menor;
 
 
-
     }
     
-
+    // Imprimimos el vector solucion teniendo en cuenta que para la implementacion
+    // La ciudad numero 1 ha sido el indice numero 0, por lo tanto tenemos que sumar
+    // 1 a los valores del vector solucion.
+    
     for(int i = 0; i< solucion.size();i++){
       cout << solucion [i]  + 1<< " --> ";
     }
-    
-    cout << "FIN";
-    
-    
+    // Añadimos la ciudad inicial para indicar que completamos un ciclo.
+   cout << " 1 " << " FIN.";
+        
+   
+   // Volcamos la salida a un fichero para su visualizacion con la ayuda de 
+   // GNUPLOT.
+   
     ofstream ficherosalida("data/ulysses_camino.txt");
     for (int i = 0; i < dimension; ++i){
         int c = solucion[i];
         ficherosalida << c+1 << " " << v[c].first << " " << v[c].second << endl;
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    
+    //FIN.
     return (0);
 }
