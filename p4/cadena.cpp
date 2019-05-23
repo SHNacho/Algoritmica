ed1f484b63987742288f10fe462ac37a21329be5
jesusjmma <jesusjmma@gmail.com>
2019-05-23 19:03:11 +0200

    modificado el codigo de la practica 4 para que sea recursivo

--- p4/cadena.cpp
+++ p4/cadena.cpp
@@ -4,104 +4,43 @@
 
 using namespace std;
 
+//Función de reconstrucción recursiva
+string reconstruccion(vector <vector <int>> matriz, string a, string b, int i, int j){
+	if (i==0 || j==0)
+		return "";
+	else if (a[i-1] == b[j-1])
+		return reconstruccion (matriz, a, b, i-1, j-1) + a[i-1];
+	else if (matriz[i-1][j]>matriz[i][j-1])
+		return reconstruccion (matriz, a, b, i-1, j);
+	else
+		return reconstruccion (matriz, a, b, i, j-1);
+}
+
+
 //Función para hallar la subsecuencia más corta con programación dinámica
 string subsecuencia(string a, string b){
 	
 	int a_tam= a.size();
 	int b_tam= b.size();
-	string subsecuencia="";
 	
 	//Creacion de la matriz con los valores y la matriz con las direcciones
 	vector <vector <int>> matriz(a_tam+1, vector <int> (b_tam+1, 0));
-	vector <vector <int>> matrizpadres(a_tam+1, vector <int> (b_tam+1, 0));
 	
 	for (int i=1; i<a_tam+1; i++){
 		for (int j=1; j<b_tam+1; j++){
-			if (a[i-1]==b[j-1]){
+			if (a[i-1]==b[j-1])
 				matriz[i][j]= matriz[i-1][j-1]+1;
-				matrizpadres[i][j]= 1;
-			}
 			else{
-				if (matriz[i-1][j]>matriz[i][j-1]){
+				if (matriz[i-1][j]>matriz[i][j-1])
 					matriz[i][j]= matriz[i-1][j];
-					matrizpadres[i][j]= 2;
-				}
-				else{
+				else
 					matriz[i][j]= matriz[i][j-1];
-					matrizpadres[i][j]= 3;
-				}
 			}
 		}
-	}
-	
-	//Impresion de la matriz con los valores
-	cout << "    ";
-	
-	for (int i=0; i<b_tam; i++)
-		cout << b[i] << " ";
-	
-	cout << endl;
-	
-	for (int i=0; i<a_tam+1; i++){
-		if(i>0)
-			cout << a[i-1] << " ";
-		else
-			cout << "  ";
-		for (int j=0; j<b_tam+1; j++){
-			cout << matriz[i][j] << " ";
-		}
-		cout << endl;
-	}
-	
-	//Impresion de la matriz con las direcciones
-	cout << endl;
-	cout << endl;
-	cout << "    ";
-	
-	for (int i=0; i<b_tam; i++)
-		cout << b[i] << " ";
-	
-	cout << endl;
-	
-	for (int i=0; i<a_tam+1; i++){
-		if(i>0)
-			cout << a[i-1] << " ";
-		else
-			cout << "  ";
-		for (int j=0; j<b_tam+1; j++){
-			if (matrizpadres[i][j]==0)
-				cout << "0 ";
-			else if (matrizpadres[i][j]==1)
-				cout << "\\ ";
-			else if (matrizpadres[i][j]==2)
-				cout << "| ";
-			else if (matrizpadres[i][j]==3)
-				cout << "- ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	
-	//Calculo de la subsecuencia
-	
-	int i= a_tam;
-	int j= b_tam;
-	
-	while (matrizpadres[i][j]!=0){
-		if (matrizpadres[i][j]==1){
-			subsecuencia= a[i-1] + subsecuencia;
-			i= i-1;
-			j= j-1;
-		}
-		else if (matrizpadres[i][j]==2)
-			i= i-1;
-		else if (matrizpadres[i][j]==3)
-			j= j-1;
-	}
-	
+	}	
 	
 	//Return
-	return subsecuencia;
+	return reconstruccion(matriz, a, b, a_tam, b_tam);
 }
 
 
