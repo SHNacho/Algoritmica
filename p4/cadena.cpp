216433b43ef80710bb2a7bbe2283bb6c9f29fdec
jesusjmma <jesusjmma@gmail.com>
2019-05-24 16:19:12 +0200

    modificado el nombre de los archivos que estaban intercambiados

--- p4/cadena.cpp
+++ p4/cadena.cpp
@@ -25,76 +25,19 @@ string subsecuencia(string a, string b){
 	
 	//Creacion de la matriz con los valores y la matriz con las direcciones
 	vector <vector <int>> matriz(a_tam+1, vector <int> (b_tam+1, 0));
-	vector <vector <int>> matrizpadres(a_tam+1, vector <int> (b_tam+1, 0));
 	
 	for (int i=1; i<a_tam+1; i++){
 		for (int j=1; j<b_tam+1; j++){
-			if (a[i-1]==b[j-1]){
+			if (a[i-1]==b[j-1])
 				matriz[i][j]= matriz[i-1][j-1]+1;
-				matrizpadres[i][j]= 1;
-			}
 			else{
-				if (matriz[i-1][j]>matriz[i][j-1]){
+				if (matriz[i-1][j]>matriz[i][j-1])
 					matriz[i][j]= matriz[i-1][j];
-					matrizpadres[i][j]= 2;
-				}
-				else{
+				else
 					matriz[i][j]= matriz[i][j-1];
-					matrizpadres[i][j]= 3;
-				}
 			}
 		}
-	}
-	
-	
-	
-	//Impresion de la matriz con los valores
-	cout << "    ";
-	
-	for (int i=0; i<b_tam; i++)
-		cout << b[i] << " ";
-	
-	cout << endl;
-	
-	for (int i=0; i<a_tam+1; i++){
-		if(i>0)
-			cout << a[i-1] << " ";
-		else
-			cout << "  ";
-		for (int j=0; j<b_tam+1; j++){
-			cout << matriz[i][j] << " ";
-		}
-		cout << endl;
-	}
-	
-	//Impresion de la matriz con las direcciones
-	cout << endl;
-	cout << endl;
-	cout << "    ";
-	
-	for (int i=0; i<b_tam; i++)
-		cout << b[i] << " ";
-	
-	cout << endl;
-	
-	for (int i=0; i<a_tam+1; i++){
-		if(i>0)
-			cout << a[i-1] << " ";
-		else
-			cout << "  ";
-		for (int j=0; j<b_tam+1; j++){
-			if (matrizpadres[i][j]==0)
-				cout << "0 ";
-			else if (matrizpadres[i][j]==1)
-				cout << "\\ ";
-			else if (matrizpadres[i][j]==2)
-				cout << "| ";
-			else if (matrizpadres[i][j]==3)
-				cout << "- ";
-		}
-		cout << endl;
-	}
-	
+	}	
 	
 	//Return
 	return reconstruccion(matriz, a, b, a_tam, b_tam);
